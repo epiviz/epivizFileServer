6f176b8a5debdbe855a722a30460403fc6e71a98
yyangdev@gmail.com <yyff1225257758@gmail.com>
2018-06-08 15:51:09 -0400

    fixed bugs

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from parser import BigWig
 
-test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")
-print(test.getRange("chrY", 0, 30, 2))
+test = BigWig("../biology-related-python-files/39033.bigwig")
+print(test.getRange("chr9", 11550488, 11554489, 10))
 
 
