aa31c02226e85ae17b39dab47330eb9f556fa7bd
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-06-09 17:42:05 -0400

    make http calls for remote files using content-range

--- parser/BaseFile.py
+++ parser/BaseFile.py
@@ -23,7 +23,7 @@ class BaseFile(object):
         self.compressed = True
 
     def is_local(self, file):
-        if "http" in file or "ftp" in file:
+        if "http://" in file or "https://" in file or "ftp://" in file:
             return False
         return True
 
@@ -33,12 +33,6 @@ class BaseFile(object):
     def get_data(self, chr, start, end):
         raise Exception("NotImplementedException")
 
-    def remote_calc_byte_range(self, chr, start, end):
-        raise Exception("NotImplementedException")
-
-    def remote_get_byte_range(self, chr, start, end):
-        raise Exception("NotImplementedException")
-
     def decompress_binary(self, bin_block):
         return zlib.decompress(bin_block)
 
@@ -46,25 +40,17 @@ class BaseFile(object):
         return ujson.dumps(data)
 
     def get_bytes(self, offset, size):
-        f = open(self.file, "rb")
-        f.seek(offset)
-        bin_value = f.read(size)
-        f.close()
-        return bin_value
-
-    def read_bytes(self, start_index, byte_size):
-        with open(self.file, "rb") as bin_file:
-            bin_file.seek(start_index)
-            bin_value = bin_file.read(byte_size)
+        if self.local:
+            f = open(self.file, "rb")
+            f.seek(offset)
+            bin_value = f.read(size)
+            f.close()
             return bin_value
+        else:
+            headers = {"Range": "bytes=%d-%d" % (offset, offset+size) }
+            resp = requests.get(self.file, headers=headers)
+            # use requests.codes.ok instead
+            if resp.status_code != 206:
+                raise Exception("URLError")
 
-    def read_parse_bytes(self, start_index, header_keys):
-        parser = {}
-        with open(self.file, "rb") as bin_file:
-            count = start_index
-            for key, value in header_keys.items():
-                bin_file.seek(count)
-                bin_value = bin_file.read(value)
-                count = count + value
-                parser[key] = bin_value
-        return parser
+            return resp.content[:size]
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from parser import BigWig
 
-test = BigWig("../biology-related-python-files/39033.bigwig")
+
+# local File
+test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")
+# print(test.getRange("chr9", 11550488, 11554489, 10))
+
+# remote File
+test = BigWig("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E071-H3K27me3.pval.signal.bigwig")
 print(test.getRange("chr9", 11550488, 11554489, 10))
-
-
