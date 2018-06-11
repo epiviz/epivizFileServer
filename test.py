038b0d9ba7e3a9121227bb1dca0b547e91aa48e6
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-06-11 08:31:41 -0400

    profile tests

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from parser import BigWig
-
+import cProfile
 
 # local File
-test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")
-# print(test.getRange("chr9", 11550488, 11554489, 10))
+print("############ local file initialization ####################")
+cProfile.run('test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")')
+print("############ local file getRange ####################")
+cProfile.run('print(test.getRange("chr9", 11550488, 11554489, 10))')
 
 # remote File
-test = BigWig("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E071-H3K27me3.pval.signal.bigwig")
-print(test.getRange("chr9", 11550488, 11554489, 10))
+print("############ remote file initialization ####################")
+# cProfile.run('test = BigWig("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E071-H3K27me3.pval.signal.bigwig")')
+cProfile.run('test = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39033.bigwig")')
+print("############ remote file getRange ####################")
+cProfile.run('print(test.getRange("chr9", 11550488, 11554489, 10))')
