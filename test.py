f259a39914512ed12c1b5670256f88a028aa9f03
shenbaiyueguang <yyff1225257758@gmail.com>
2018-06-13 15:27:08 +0800

    fixed some more bugs

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from parser import BigWig
 
 
 # local File
-# test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")
-# print(test.getRange("chr9", 11550488, 11554489, 2))
+test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")
+print(test.getRange("chr9", 10550488, 11554489, 10))
 
 # remote File
-test = BigWig("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E071-H3K27me3.pval.signal.bigwig")
-print(test.getRange("chr9", 11550488, 11554489, 10))
+# test = BigWig("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E071-H3K27me3.pval.signal.bigwig")
+# print(test.getRange("chr9", 11550488, 11554489, 10))
