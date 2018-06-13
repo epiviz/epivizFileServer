47d63eeb0ff235e6fb3df4438eef3b75e7ce2d26
shenbaiyueguang <yyff1225257758@gmail.com>
2018-06-13 15:33:00 +0800

    added testResult

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import cProfile
 # local File
 
 print("############ local file initialization ####################")
-# cProfile.run('test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")')
-cProfile.run('test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")')
+cProfile.run('test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")')
+# cProfile.run('test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")')
 print("############ local file getRange ####################")
-cProfile.run('print(test.getRange("chr9", 11550488, 11554489, 10))')
+cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10))')
 
 # remote File
 print("############ remote file initialization ####################")
 cProfile.run('test = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39033.bigwig")')
 print("############ remote file getRange ####################")
-cProfile.run('print(test.getRange("chr9", 11550488, 11554489, 10))')
+cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10))')
