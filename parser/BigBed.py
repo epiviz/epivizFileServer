186dc7477d149512e8fd25515a5f6fab692b6073
yyangdev@gmail.com <yyff1225257758@gmail.com>
2018-06-14 15:44:46 -0400

    bigbed support. Sync for bug fix.

---
+++ parser/BigBed.py
@@ -0,0 +1,76 @@
+from .BigWig import BigWig
+
+class BigBed(BigWig):
+    """
+        File BigBed class
+    """
+    magic = "0x8789F2EB"
+
+    def __init__(self, file):
+        super(BigBed, self).__init__(file)
+        self.zoomOffset = 0
+
+    def getRange(self, chr, start, end):
+        if start >= end:
+            raise Exception("InputError")
+
+        self.tree = self.getTree()
+        valueArray = self.getValues(chr, start, end)
+
+        return valueArray
+
+
+    # placeholder because of super function
+    def grepAnnoyingSections(self, dataOffset, dataSize, chrmId, startIndex, endIndex):
+        data = self.get_bytes(dataOffset, dataSize)
+        decom = zlib.decompress(data) if self.compressed else data
+        result = []
+        x = 0
+        length = len(decom)
+        while x < length:
+            (chromId, start, end) = struct.unpack("III", decom[x:x + 12])
+            x += 11
+            if chromId == chrmId:
+                value = ""
+                while x < length and not decom[x + 1] == "\0":
+                    x += 1
+                    value += chr(struct.unpack("b", decom[x])[0])
+                x += 2
+                if start > endIndex:
+                    pass
+                elif endIndex - 1 <= end:
+                    result.append((startIndex, endIndex - 1, value))
+                    startIndex = endIndex
+                elif end > startIndex:
+                    result.append((startIndex, end, value))
+                    startIndex = end + 1
+            else:
+                while x < length and not decom[x + 1] == "\0":
+                    x += 1
+                x += 2
+        return startIndex, result
+
+    def grepSections(self, dataOffset, dataSize, startIndex, endIndex):
+        data = self.get_bytes(dataOffset, dataSize)
+        decom = zlib.decompress(data) if self.compressed else data
+        result = []
+        x = 0
+        length = len(decom)
+
+        while x < length:
+            (chromId, start, end) = struct.unpack("III", decom[x:x + 12])
+            x += 11
+            value = ""
+            while x < length and not decom[x + 1] == "\0":
+                x += 1
+                value += chr(struct.unpack("b", decom[x])[0])
+            x += 2
+            if start > endIndex:
+                pass
+            elif endIndex - 1 <= end:
+                result.append((startIndex, endIndex - 1, value))
+                startIndex = endIndex
+            elif end > startIndex:
+                result.append((startIndex, end, value))
+                startIndex = end + 1
+        return startIndex, result
--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 from parser import BigWig
+from parser import BigBed
 import cProfile
 
 # local File
 
 print("############ local file initialization ####################")
 # cProfile.run('test = BigWig("/home/jayaram/.AnnotationHub/39033.bigwig")')
-cProfile.run('test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")')
-print("############ local file getRange ####################")
-cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10, 3))')
+# cProfile.run('test = BigWig("/home/evan/Desktop/epiviz/39033.bigwig")')
+# print("############ local file getRange ####################")
+# cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10, 3))')
 
-# remote File
-print("############ remote file initialization ####################")
-cProfile.run('test = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39033.bigwig")')
-print("############ remote file getRange ####################")
-cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10, 3))')
-
+# # remote File
+# print("############ remote file initialization ####################")
+# cProfile.run('test = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39033.bigwig")')
+# print("############ remote file getRange ####################")
+# cProfile.run('print(test.getRange("chr9", 10550488, 11554489, 10, 3))')
+test = BigBed("/Users/evan/python/justBioThings/test.bigBed")
+print(test.getRange("chr9", 10009333, 10009350))
