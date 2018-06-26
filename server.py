b14d772aa14fdcf4f500cbaf050e74493ba46703
shenbaiyueguang <yyff1225257758@gmail.com>
2018-06-26 14:05:17 +0800

    merge

--- server.py
+++ server.py
@@ -1,37 +1,50 @@
-from flask import Flask, request, g
-from handler import FileHandler
-app = Flask(__name__)
+# from flask import Flask, request, g
+# 
+# app = Flask(__name__)
 
-@app.before_first_request
-def startup():
-    g.ph = FileHandler()
-    print("FileHandler created")
+# @app.before_first_request
+# def startup():
+#     g.ph = FileHandler()
+#     print("FileHandler created")
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+# @app.route("/getBigWigData")
+# def getBigWigData():
+# 	fileName = request.args.get('name')
+# 	chrom = request.args.get('chr')
+# 	startIndex = int(request.args.get('start'))
+# 	endIndex = int(request.args.get('end'))
+# 	points = int(request.args.get('points')) if request.args.get('points') != None else 2000
+# 	result = g.ph.handleBigWig(fileName, chrom, startIndex, endIndex, points)
+	
+# 	return str(result)
+# 	# return str(fileName, chrom, startIndex, endIndex, points)
+# 	# return request.query_string
+
+# @app.route("/getBigBedData")
+# def getBigWigData():
+# 	fileName = request.args.get('name')
+# 	chrom = request.args.get('chr')
+# 	startIndex = int(request.args.get('start'))
+# 	endIndex = int(request.args.get('end'))
+# 	result = g.ph.handleBigBed(fileName, chrom, startIndex, endIndex)
+	
+# 	return str(result)
+# 	# return str(fileName, chrom, startIndex, endIndex, points)
+# 	# return request.query_string
+
+from sanic import Sanic
+from sanic.response import json
+from handler import FileHandler
+
+app = Sanic()
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+async def test(request):
+    return json({"hello": "world"})
 
-@app.route("/getBigWigData")
-def getBigWigData():
-	fileName = request.args.get('name')
-	chrom = request.args.get('chr')
-	startIndex = int(request.args.get('start'))
-	endIndex = int(request.args.get('end'))
-	points = int(request.args.get('points')) if request.args.get('points') != None else 2000
-	result = g.ph.handleBigWig(fileName, chrom, startIndex, endIndex, points)
-	
-	return str(result)
-	# return str(fileName, chrom, startIndex, endIndex, points)
-	# return request.query_string
-
-@app.route("/getBigBedData")
-def getBigWigData():
-	fileName = request.args.get('name')
-	chrom = request.args.get('chr')
-	startIndex = int(request.args.get('start'))
-	endIndex = int(request.args.get('end'))
-	result = g.ph.handleBigBed(fileName, chrom, startIndex, endIndex)
-	
-	return str(result)
-	# return str(fileName, chrom, startIndex, endIndex, points)
-	# return request.query_string
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
