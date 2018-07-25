be47a903ea4cfaee7c95cd848f20ec0127ac437c
Yifan Yang <yyff1225257758@gmail.com>
2018-07-25 12:01:18 +0800

    db support for bigbed format

--- handler/FileHandlerProcess.py
+++ handler/FileHandlerProcess.py
@@ -10,10 +10,6 @@ import concurrent.futures
 import threading
 import handler.utils as utils
 
-# ----- things to be done -------
-# remove cache and db cache at start
-# fix bug
-# add support for non-consequtive range after sql search
 dbUsername = 'root'
 dbPassword = '123123123'
 
@@ -29,6 +25,7 @@ class FileHandlerProcess(object):
         self.ManagerLock = Lock()
         self.counter = 0
         self.dbConnection = {}
+        self.inProgress = {}
 
         # if in the future we switch to python 3.7
         # self.executor = concurrent.futures.ThreadPoolExecutor(max_workers = MAXWORKER, initializer = self.threadInit, initargs=())
@@ -48,14 +45,14 @@ class FileHandlerProcess(object):
         c.execute('''DROP TABLE IF EXISTS cache''')
 
         #self.c.execute('''CREATE TABLE cache
-        #     (fileId integer, lastTime timestamp, zoomLvl integer, startI integer, endI integer, chrom text, valueBW real, valueBB text,
-        #      UNIQUE(fileId, zoomLvl, startI, endI))''')
+        #     (fileId integer, lastTime timestamp, zoomlvl integer, startI integer, endI integer, chrom text, valueBW real, valueBB text,
+        #      UNIQUE(fileId, zoomlvl, startI, endI))''')
         c.execute('''CREATE TABLE cache
-             (fileId int, lastTime timestamp, zoomLvl int, startI bigint, endI bigint, chrom varchar(255), valueBW double, valueBB double,
-             UNIQUE(fileId, zoomLvl, startI, endI))''')
+             (fileId int, lastTime timestamp, zoomlvl int, startI bigint, endI bigint, chrom varchar(255), valueBW double, valueBB varchar(255),
+             UNIQUE(fileId, zoomlvl, startI, endI))''')
         # c.execute('''ALTER TABLE  cache 
         #             ADD UNIQUE indexname
-        #             (fileId, zoomLvl, startI, endI);''')
+        #             (fileId, zoomlvl, startI, endI);''')
         self.mainConnection.commit()
         c.close()
 
@@ -124,23 +121,23 @@ class FileHandlerProcess(object):
 
         return record["fileObj"], record["ID"]
 
-    def sqlQueryBW(self, startIndex, endIndex, chrom, zoomLvl, fileId):
+    def sqlQuery(self, startIndex, endIndex, chrom, zoomlvl, fileId, col):
         result = []
         start = []
         end = []
         c = self.getConnection(threading.get_ident()).cursor()
-        # for row in self.c.execute('SELECT startI, endI, valueBW FROM cache WHERE (fileId=%s AND zoomLvl=%s AND startI>=%s AND endI<=%s AND chrom=%s)', 
-        #     (fileId, zoomLvl, startIndex, endIndex, chrom)):
+        # for row in self.c.execute('SELECT startI, endI, valueBW FROM cache WHERE (fileId=%s AND zoomlvl=%s AND startI>=%s AND endI<=%s AND chrom=%s)', 
+        #     (fileId, zoomlvl, startIndex, endIndex, chrom)):
             # result.append((row[0], row[1], row[2]))
             # # calculate missing range
             # if row[0] > startIndex:
             #     start.append(startIndex)
             #     end.append(row[0])
             # startIndex = row[1]
-        c.execute('SELECT startI, endI, valueBW FROM cache WHERE (fileId=%s AND zoomLvl=%s AND startI>=%s AND endI<=%s AND chrom=%s)', 
-            (fileId, zoomLvl, startIndex, endIndex, chrom))
+        c.execute('SELECT startI, endI, %s FROM cache WHERE (fileId=%s AND zoomlvl=%s AND startI>=%s AND endI<=%s AND chrom=%s)', 
+            (col, fileId, zoomlvl, startIndex, endIndex, chrom))
         for row in c.fetchall():
-            result.append((row["startI"], row["endI"], row["valueBW"]))
+            result.append((row["startI"], row["endI"], row[col]))
             # calculate missing range
             if row["startI"] > startIndex:
                 start.append(startIndex)
@@ -151,72 +148,49 @@ class FileHandlerProcess(object):
         c.close()
         return start, end, result
 
-    def addToDbBW(self, result, chrom, fileId, zoomLvl):
+    def addToDb(self, result, chrom, fileId, zoomlvl, col):
         c = self.getConnection(threading.get_ident()).cursor()
         # for s, e, v in zip(result.gets("start"), result.gets("end"), result.gets("value")):
         for r in result:
             for s in r:
+                if col is "valueBB":
+                    bb = s[2]
+                    bw = 0
+                else:
+                    bw = s[2]
+                    bb = 0
                 c.execute("INSERT INTO cache VALUES (%s, %s, %s, %s, %s, %s, %f, %f) ON DUPLICATE KEY UPDATE lastTime = %s", 
-                    (fileId, datetime.now(), zoomLvl, s[0], s[1], chrom, s[2], 0, datetime.now()))
-                # c.execute("UPDATE cache SET lastTime = %s WHERE (fileId=%s AND zoomLvl=%s AND startI=%s AND endI=%s AND chrom=%s)",
-                #     (datetime.now(), fileId, zoomLvl, s[0], s[1], chrom))
+                    (fileId, datetime.now(), zoomlvl, s[0], s[1], chrom, bw, bb, datetime.now()))
+                # c.execute("UPDATE cache SET lastTime = %s WHERE (fileId=%s AND zoomlvl=%s AND startI=%s AND endI=%s AND chrom=%s)",
+                #     (datetime.now(), fileId, zoomlvl, s[0], s[1], chrom))
         self.connection.commit()
         c.close()
 
-    def bigwigWrapper(self, fileObj, chrom, startIndex, endIndex, points, fileId):
+    def bigwigWrapper(self, fileObj, chrom, startIndex, endIndex, points, fileId, zoomlvl):
         f=[]
         result = []
-        points = (endIndex - startIndex) if points > (endIndex - startIndex) else points
-        step = (endIndex - startIndex)*1.0/points
-        f.append(self.executor.submit(fileObj.getRange, chrom, startIndex, endIndex))
-        zoomLvl, _ = self.executor.submit(fileObj.getZoom, step).result()
-        m = self.executor.submit(self.sqlQueryBW, startIndex, endIndex, chrom, zoomLvl, fileId)
-        concurrent.futures.as_completed([m])
-        (start, end, dbRusult) = m.result()
+        (start, end, dbRusult) = self.sqlQuery(startIndex, endIndex, chrom, zoomlvl, fileId, "valueBW")
         for s, e in zip(start, end):
-            f.append(self.executor.submit(fileObj.getRange, chrom, s, e, zoomlvl = zoomLvl))
-            result.append(fileObj.getRange(chrom, s, e, zoomlvl = zoomLvl))
+            f.append(self.executor.submit(fileObj.getRange, chrom, s, e, zoomlvl = zoomlvl))
         for t in concurrent.futures.as_completed(f):
             result.append(t.result())
-        self.executor.submit(self.addToDbBW, result, chrom, fileId, zoomLvl)
+        self.executor.submit(self.addToDb, result, chrom, fileId, zoomlvl, "valueBW")
         # asyncio.ensure_future(addToDb)
         # return await self.mergeBW(result, dbRusult)
         result.append(dbRusult)
         return self.merge(result)
 
-    def sqlQueryBB(self, startIndex, endIndex, chrom, fileId):
+    async def bigbedWrapper(self, fileObj, chrom, startIndex, endIndex, fileId, zoomlvl = -2):
+        f=[]
         result = []
-        start = []
-        end = []
-        for row in self.c.execute('SELECT startI, endI, valueBB FROM cache WHERE (fileId=%s AND startI>=%s AND endI<=%s AND chrom=%s)', 
-            (fileId, startIndex, endIndex, chrom)):
-            result.append((row[0], row[1], row[2]))
-            # calculate missing range
-            if row[0] > startIndex:
-                start.append(startIndex)
-                end.append(row[0])
-            startIndex = row[1]
-        start.append(startIndex)
-        end.append(endIndex)
-
-        return start, end, result
-
-    async def addToDbBB(self, result, chrom, fileId):
-        # for s, e, v in zip(result.gets("start"), result.gets("end"), result.gets("value")):
-        for r in result:
-            for s in r:
-                self.c.execute("INSERT OR IGNORE INTO cache VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                    (fileId, datetime.now(), -2, s[0], s[1], chrom, 0.0, s[2]))
-                self.c.execute("UPDATE cache SET lastTime = %s WHERE (fileId=%s AND startI=%s AND endI=%s AND chrom=%s)",
-                    (datetime.now(), fileId, s[0], s[1], chrom))
-        self.db.commit()
-
-    async def bigbedWrapper(self, fileObj, chrom, startIndex, endIndex, fileId):
-        result=[]
-        (start, end, dbRusult) = await self.sqlQueryBB(startIndex, endIndex, chrom, fileId)
+        (start, end, dbRusult) = self.sqlQuery(startIndex, endIndex, chrom, zoomlvl, fileId, "valueBB")
         for s, e in zip(start, end):
-            result.append(await fileObj.getRange(chrom, s, e))
-        asyncio.ensure_future(self.addToDbBB(result, chrom, fileId))
+            f.append(self.executor.submit(fileObj.getRange, chrom, s, e, zoomlvl = zoomlvl))
+        for t in concurrent.futures.as_completed(f):
+            result.append(t.result())
+        self.executor.submit(self.addToDb, result, chrom, fileId, zoomlvl, "valueBB")
+        # asyncio.ensure_future(addToDb)
+        # return await self.mergeBW(result, dbRusult)
         result.append(dbRusult)
         return self.merge(result)
 
@@ -226,21 +200,31 @@ class FileHandlerProcess(object):
         l.sort(key=g)
         return l
 
+    # search for any overlap that is already in progress, returns the updated start and end index,
+    # and a future array containing overlapped results.
+    def locateRedundent(self, fileId, startIndex, endIndex, zoomlvl):
+        futures = []
+        with self.inProgress.get(fileId) as f:
+            if f is not None:
+                with f.get(zoomlvl) as z:
+                    if z is not None:
+                        pass
+
+        return startIndex, endIndex, futures
+
+    def updateInprogress(self, m, fileId, zoomlvl, start, end):
+        pass
+
     async def handleBigWig(self, fileName, chrom, startIndex, endIndex, points):
         if self.records.get(fileName) == None:
-            # p = Process(target=f, args=(d, l))
-            # p = BieWigProcess(fileName, "BW")
             bigwig = BigWig(fileName)
             bigwig.getHeader()
             fileId = self.setManager(fileName, bigwig)
         else:
             bigwig, fileId = self.updateTime(fileName)
-        # p.start(chrom, startIndex, endIndex, points)
         loop = asyncio.get_event_loop()
-        m = await loop.run_in_executor(self.executor, self.bigwigWrapper, bigwig, chrom, startIndex, endIndex, points, fileId)
-        # f = self.executor.submit(self.bigwigWrapper, bigwig, chrom, startIndex, endIndex, points, fileId)
-        # m=await f.result()
-        return m
+        m = loop.run_in_executor(self.executor, self.bigwigWrapper, bigwig, chrom, startIndex, endIndex, points, fileId, zoomlvl)
+        return await m
 
     async def handleBigBed(self, fileName, chrom, startIndex, endIndex):
         if self.records.get(fileName) == None:
@@ -268,9 +252,19 @@ class FileHandlerProcess(object):
 
         if fileType in ["bigwig", "bigWig", "BigWig", "bw"]:
             wrapper = self.bigwigWrapper
+            points = (endIndex - startIndex) if points > (endIndex - startIndex) else points
+            step = (endIndex - startIndex)*1.0/points
+            zoomlvl, _ = self.executor.submit(fileObj.getZoom, step).result()
+            # start, end, futures = self.locateRedundent(fileId, startIndex, endIndex, zoomlvl)
         elif fileType in ["bigbed", "bb", "BigBed", "bigBed"]:
             wrapper = self.bigbedWrapper
+            zoomlvl = -2
+            # start, end, futures = self.locateRedundent(fileId, startIndex, endIndex, zoomlvl)
 
-        m = await loop.run_in_executor(self.executor, wrapper, fileObj, 
-                                    chrom, startIndex, endIndex, points, fileId)
-        return m
+        
+        m = loop.run_in_executor(self.executor, wrapper, fileObj, 
+                                    chrom, startIndex, endIndex, points, fileId, zoomlvl)
+        # result = [self.updateInprogress(m, fileId, zoomlvl, start, end)]
+        # for f in concurrent.futures.as_completed(futures):
+        #     result.append(f.result())
+        return await m
