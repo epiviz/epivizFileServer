fd91177a8851c40d0d71091f032a4c11393f6605
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-08-24 11:35:10 -0400

    refactoring parser module. optimize rtree traversal and leaf node parsing

--- parser/BigBed.py
+++ parser/BigBed.py
@@ -10,98 +10,33 @@ class BigBed(BigWig):
     magic = "0x8789F2EB"
     def __init__(self, file):
         super(BigBed, self).__init__(file)
-        # self.zoomOffset = 0
-        self.zoomOffset[os.getpid()] = 0
 
-    async def getRange(self, chr, start, end, zoomlvl = -2, respType = "JSON"):
-        if not hasattr(self, 'header'):
-            await self.getHeader()
-
-        if start > end:
-            raise Exception("InputError")
-        elif start is end:
-            return []
-        if self.tree.get(-2) == None:    
-            self.tree = await self.getTree(-2)
-        # self.tree = self.getTree()
-
-        value = []
-        startArray = []
-        endArray = []
-
-        valueArray = await self.getValues(chr, start, end, -2)
-
-        for item in valueArray:
-            startArray.append(item[0])
-            endArray.append(item[1])
-            value.append(item[2])
-
-        if respType is "JSON":
-            formatFunc = self.formatAsJSON
-
-        # self.tree = None
-        # return formatFunc({"start" : startArray, "end" : endArray, "values": value})
-        return valueArray
-
-    # placeholder because of super function
-    async def grepAnnoyingSections(self, dataOffset, dataSize, chrmId, startIndex, endIndex):
-        data = await self.get_bytes(dataOffset, dataSize)
+    def parseLeafDataNode(self, chrmId, start, end, zoomlvl, rStartChromIx, rStartBase, rEndChromIx, rEndBase, rdataOffset, rDataSize):
+        data = self.get_bytes(rdataOffset, rDataSize)
         decom = zlib.decompress(data) if self.compressed else data
         result = []
         x = 0
         length = len(decom)
-        while x < length:
-            (chromId, start, end) = struct.unpack("III", decom[x:x + 12])
-            x += 11
-            if chromId == chrmId:
-                value = ""
-                while x < length and not decom[x + 1] == "\0":
+        while x < length and x+12 < length:
+            (chrmIdv, startv, endv) = struct.unpack(self.endian + "III", decom[x:x + 12])
+            x += 12
+            if chrmIdv == chrmId:
+                valuev = ""
+                while x < length:
+                    (tempv) = struct.unpack(self.endian + "c", decom[x:x+1])
+                    (tempNext) = struct.unpack(self.endian + "c", decom[x+1:x+2])
+                    valuev += str(tempv[0].decode())
+                    if tempNext[0].decode() == '\x00': 
+                        if startv <= end:
+                            result.append((chrmIdv, startv, endv, valuev))
+                        break
                     x += 1
-                    # need to change if switched to python 3
-                    value += (decom[x])
-                x += 2
-                if start > endIndex:
-                    pass
-                elif endIndex - 1 <= end:
-                    result.append((startIndex, endIndex - 1, value))
-                    startIndex = endIndex
-                elif end > startIndex:
-                    result.append((startIndex, end, value))
-                    startIndex = end + 1
             else:
-                while x < length and not decom[x + 1] == "\0":
+                while x < length:
+                    (tempv) = struct.unpack(self.endian + "c", decom[x:x+1])
+                    (tempNext) = struct.unpack(self.endian + "c", decom[x+1:x+2])
+                    if tempNext[0].decode() == '\x00': 
+                        break
                     x += 1
-                x += 2
-        return startIndex, result
-
-    async def grepSections(self, dataOffset, dataSize, startIndex, endIndex, zoomlvl):
-        data = await self.get_bytes(dataOffset, dataSize)
-        decom = zlib.decompress(data) if self.compressed else data
-        result = []
-        x = 0
-        length = len(decom)
-
-        while x < length and startIndex < endIndex:
-            (chromId, start, end) = struct.unpack("III", decom[x:x + 12])
-            x += 11
-            value = ""
-            while x < length and not decom[x + 1] == "\0":
-                x += 1
-                # need to change if switched to python 3
-                value += (decom[x])
             x += 2
-            if start > endIndex:
-                pass
-            elif endIndex - 1 <= end:
-                # for exclusive endIndex return
-                result.append((startIndex, endIndex, value))
-                # for inclusive endIndex return
-                # result.append((startIndex, endIndex - 1, value))
-                startIndex = endIndex
-            elif end > startIndex:
-                # for exclusive endIndex return
-                result.append((startIndex, end + 1, value))
-                # for inclusive endIndex return
-                # result.append((startIndex, end, value))
-                startIndex = end + 1
-        return startIndex, result
+        return result
