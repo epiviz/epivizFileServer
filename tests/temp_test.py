7f29cfb37847b48d095ce25ecb601a4e1270997d
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2019-09-04 16:45:05 -0400

    update tests

--- tests/temp_test.py
+++ tests/temp_test.py
@@ -1,8 +1,8 @@
-from parser import *
-import pysam
-path = "https://obj.umiacs.umd.edu/bigwig-files/hg38.gtf.gz"
-f = GtfFile(path)
-# pf = pysam.TabixFile(path)
-# for record in pf.fetch():
-#   print(record)
-print(f.getRange("1", 1, 100000))
+# from parser import *
+# import pysam
+# path = "https://obj.umiacs.umd.edu/bigwig-files/hg38.gtf.gz"
+# f = GtfFile(path)
+# # pf = pysam.TabixFile(path)
+# # for record in pf.fetch():
+# #   print(record)
+# print(f.getRange("1", 1, 100000))
--- tests/testClient.py
+++ tests/testClient.py
@@ -1,14 +1,14 @@
 from epivizfileserver.client import EpivizClient
 
-ee = EpivizClient("http://localhost:8000", "files")
-m = ee.get_measurements()
+# ee = EpivizClient("http://localhost:8000", "files")
+# m = ee.get_measurements()
 
-# print(m)
-m1 = m[0]
-print("hello")
-print(m1)
+# # print(m)
+# m1 = m[0]
+# print("hello")
+# print(m1)
 
-res = m1.get_data("chr11", 133161646, 135928951)
+# res = m1.get_data("chr11", 133161646, 135928951)
 
-print(res)
+# print(res)
 
