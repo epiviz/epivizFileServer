6c703bd81c5af61159e287c0b75dddfd4d6d258d
hcorrada <hcorrada@gmail.com>
2020-07-31 09:38:47 -0700

    refactor import methods; add an import_emd method

---
+++ scripts/test_md-endpoint.py
@@ -0,0 +1,12 @@
+from epivizfileserver import setup_app, MeasurementManager, create_fileHandler
+
+emd_url = 'http://localhost/emd/api/v1/'
+
+mMgr = MeasurementManager()
+handler = create_fileHandler()
+mMgr.import_emd(emd_url, fileHandler=handler, listen=False)
+
+app = setup_app(mMgr)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
